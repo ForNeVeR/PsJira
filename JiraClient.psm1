8abafdaba9fcb38b2a600b35ccc3234b810c231a
ForNeVeR <neverthness@gmail.com>
2013-04-22 11:52:10 +0700

    Add Get-JiraIssue command.

--- JiraClient.psm1
+++ JiraClient.psm1
@@ -1,5 +1,5 @@
 function Get-JiraIssueStatus {
-    param(
+	param(
 		[Parameter(Mandatory = $true)]
 		$Url,
 
@@ -11,43 +11,89 @@ function Get-JiraIssueStatus {
 
 		[Parameter(ValueFromPipeLine = $true)]
 		$IssueKeys
-    )
+	)
 
-    begin { 
-    	$wsdl = "$Url/rpc/soap/jirasoapservice-v2?wsdl"
-	    $proxy = New-WebServiceProxy $wsdl
-	    Write-Host "Fetching WSDL from $wsdl"
+	begin { 
+		$wsdl = "$Url/rpc/soap/jirasoapservice-v2?wsdl"
+		$proxy = New-WebServiceProxy $wsdl
+		Write-Host "Fetching WSDL from $wsdl"
 
-	    $token = $proxy.login($Login, $Password)
-	    if ($token) {
-	    	Write-Host "Succesfully logged in as $Login"
-	    }
+		$token = $proxy.login($Login, $Password)
+		if ($token) {
+			Write-Host "Succesfully logged in as $Login"
+		}
 
-	    Write-Host 'Fetching statuses...'
-    	$statuses = $proxy.getStatuses($token)
-    }
+		Write-Host 'Fetching statuses...'
+		$statuses = $proxy.getStatuses($token)
+	}
 
-    process {
-    	foreach ($issueKey in $IssueKeys) {
-    		Write-Host "Fetching issue $IssueKey"
-	    	$issue = $proxy.getIssue($token, $IssueKey)
-	    	if (-not $issue) {
-	    		Write-Error "Issue $issueKey not found"
-	    		continue
-	    	}
+	process {
+		foreach ($issueKey in $IssueKeys) {
+			Write-Host "Fetching issue $IssueKey"
+			$issue = $proxy.getIssue($token, $IssueKey)
+			if (-not $issue) {
+				Write-Error "Issue $issueKey not found"
+				continue
+			}
 
-	    	$statusId = $issue.status
-	    	$status = $statuses | ? { $_.id -eq $statusId }
+			$statusId = $issue.status
+			$status = $statuses | ? { $_.id -eq $statusId }
 
-	    	$info = [pscustomobject] @{ Key = $issueKey; Status = $status.name }
-	    	$info
-    	}
-    }
+			$info = [pscustomobject] @{ Key = $issueKey; Status = $status.name }
+			$info
+		}
+	}
 
-    end {
-    	$proxy.logout($token)
+	end {
+		$proxy.logout($token)
    		Write-Host 'Successfully logged out'
-    }
+	}
+}
+
+function Get-JiraIssue {
+	param(
+		[Parameter(Mandatory = $true)]
+		$Url,
+
+		[Parameter(Mandatory = $true)]
+		$Login,
+
+		[Parameter(Mandatory = $true)]
+		$Password,
+
+		[Parameter(ValueFromPipeLine = $true)]
+		$IssueKeys
+	)
+
+	begin { 
+		$wsdl = "$Url/rpc/soap/jirasoapservice-v2?wsdl"
+		$proxy = New-WebServiceProxy $wsdl
+		Write-Host "Fetching WSDL from $wsdl"
+
+		$token = $proxy.login($Login, $Password)
+		if ($token) {
+			Write-Host "Succesfully logged in as $Login"
+		}
+	}
+
+	process {
+		foreach ($issueKey in $IssueKeys) {
+			Write-Host "Fetching issue $IssueKey"
+			$issue = $proxy.getIssue($token, $IssueKey)
+			if (-not $issue) {
+				Write-Error "Issue $issueKey not found"
+				continue
+			}
+
+			$issue
+		}
+	}
+
+	end {
+		$proxy.logout($token)
+		Write-Host 'Successfully logged out'
+	}
 }
 
 Export-ModuleMember Get-JiraIssueStatus
+Export-ModuleMember Get-JiraIssue
